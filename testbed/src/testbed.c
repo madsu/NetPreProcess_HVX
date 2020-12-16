213af9a6f24d2d6d31e477e49be5cce407690883
wangzewei <wangzewei@tetras.ai>
2020-12-16 19:51:37 +0800

    add common

--- testbed/src/testbed.c
+++ testbed/src/testbed.c
@@ -2,6 +2,14 @@
 
 #include <stdio.h>
 
+#if defined(__hexagon__)
+#include "hexagon_standalone.h"
+#include "subsys.h"
+#endif
+
+#include "io.h"
+#include "hvx.cfg.h"
+
 void PrintVec(short *vec, unsigned int len)
 {
     printf("print vec: ");
@@ -13,6 +21,8 @@ void PrintVec(short *vec, unsigned int len)
 
 int main(int argc, char **argv)
 {
+    long long start_time, total_cycles;
+
     unsigned int len = 32 * 10;
     short *vec_buf = (short *)malloc(sizeof(short) * len);
 
@@ -21,9 +31,30 @@ int main(int argc, char **argv)
         vec_buf[i] = (short)(rand());
     }
 
+#if defined(__hexagon__)
+    subsys_enable();
+    SIM_ACQUIRE_HVX;
+#if LOG2VLEN == 7
+    SIM_SET_HVX_DOUBLE_MODE;
+#endif
+#endif 
+
+    /* -----------------------------------------------------*/
+    /*  Call fuction                                        */
+    /* -----------------------------------------------------*/
     PrintVec(vec_buf, len);
+
+    RESET_PMU();
+    start_time = READ_PCYCLES();
+
     vec_abs(vec_buf, len);
+
+    total_cycles = READ_PCYCLES() - start_time;
+    DUMP_PMU();
+
     PrintVec(vec_buf, len);
+
+    printf("run cycles %d\n", total_cycles);
 
     free(vec_buf);
     return 0;
